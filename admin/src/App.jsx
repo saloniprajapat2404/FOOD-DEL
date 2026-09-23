79958034e516a52881f878ea29eafbb88c74cce6
saloniprajapat2404 <saloniprajapat977@gmail.com>
2026-09-23 23:35:25 +0530

    Replace old backend URL

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
 
-  const url ="https://food-del-backend-48p7.onrender.com"
+  const url = "https://food-del-qcum.onrender.com"
   return (
     <div>
       <ToastContainer/>
